41289ff08993530db317b97a87f2b8038ca2c1ca
Daniel Doubrovkine <dblock@dblock.org>
2009-04-11 14:03:27 +0000

    Moved "Getting Started" samples into a new VMWareLibSamples.
    git-svn-id: https://vmwaretasks.svn.codeplex.com/svn/trunk@21723 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ Source/VMWareLibSamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VMWareLibSamples")]
+[assembly: Guid("2c39b3fe-fbf5-458b-ac07-9c1630a1d14c")]
---
+++ Source/VMWareLibSamples/VMWareVirtualHostSamples.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vestris.VMWareLib;
+
+namespace VMWareLibSamples
+{
+    public class VMWareVirtualHostSamples
+    {
+        public void GettingStartedWorkstation()
+        {
+            #region Example: Getting Started (Workstation)
+            // declare a virtual host
+            VMWareVirtualHost virtualHost = new VMWareVirtualHost();
+            // connect to a local (VMWare Workstation) virtual machine
+            virtualHost.ConnectToVMWareWorkstation();
+            // open an existing virtual machine
+            VMWareVirtualMachine virtualMachine = virtualHost.Open(@"C:\Virtual Machines\xp\xp.vmx");
+            // power on this virtual machine
+            virtualMachine.PowerOn();
+            // wait for VMWare Tools
+            virtualMachine.WaitForToolsInGuest();
+            // login to the virtual machine
+            virtualMachine.LoginInGuest("Administrator", "password");
+            // run notepad
+            virtualMachine.RunProgramInGuest("notepad.exe", string.Empty);
+            // create a new snapshot
+            string name = "New Snapshot";
+            // take a snapshot at the current state
+            virtualMachine.Snapshots.CreateSnapshot(name, "test snapshot");
+            // power off
+            virtualMachine.PowerOff();
+            // find the newly created snapshot
+            VMWareSnapshot snapshot = virtualMachine.Snapshots.GetNamedSnapshot(name);
+            // revert to the new snapshot
+            snapshot.RevertToSnapshot();
+            // delete snapshot
+            snapshot.RemoveSnapshot();
+            #endregion
+        }
+
+        public void GettingStartedVI()
+        {
+            #region Example: Getting Started (VI)
+            // declare a virtual host
+            VMWareVirtualHost virtualHost = new VMWareVirtualHost();
+            // connect to a remove (VMWare ESX) virtual machine
+            virtualHost.ConnectToVMWareVIServer("esx.mycompany.com", "vmuser", "password");
+            // open an existing virtual machine
+            VMWareVirtualMachine virtualMachine = virtualHost.Open("[storage] testvm/testvm.vmx");
+            // power on this virtual machine
+            virtualMachine.PowerOn();
+            // wait for VMWare Tools
+            virtualMachine.WaitForToolsInGuest();
+            // login to the virtual machine
+            virtualMachine.LoginInGuest("Administrator", "password");
+            // run notepad
+            virtualMachine.RunProgramInGuest("notepad.exe", string.Empty);
+            // create a new snapshot
+            string name = "New Snapshot";
+            // take a snapshot at the current state
+            virtualMachine.Snapshots.CreateSnapshot(name, "test snapshot");
+            // power off
+            virtualMachine.PowerOff();
+            // find the newly created snapshot
+            VMWareSnapshot snapshot = virtualMachine.Snapshots.GetNamedSnapshot(name);
+            // revert to the new snapshot
+            snapshot.RevertToSnapshot();
+            // delete snapshot
+            snapshot.RemoveSnapshot();
+            #endregion
+        }
+    }
+}
