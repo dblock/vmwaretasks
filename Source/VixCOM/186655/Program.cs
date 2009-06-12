4f968464115793b161662932f829d5c2c379f6da
Daniel Doubrovkine <dblock@dblock.org>
2009-06-12 22:34:58 +0000

    Fixed wrong comment and removed infinite loop.
    git-svn-id: https://vmwaretasks.svn.codeplex.com/svn/trunk@25095 9b283d60-5439-405e-af05-b73fd8c4d996

--- Source/VixCOM/186655/Program.cs
+++ Source/VixCOM/186655/Program.cs
@@ -13,70 +13,65 @@ namespace VMWareCrash
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            while (true)
+            try
             {
-                try
+                VixCOM.VixLib vix = new VixLib();
+
                 {
-                    VixCOM.VixLib vix = new VixLib();
+                    // connect to a VI host
+                    Console.WriteLine("Connecting to VI host");
+                    IJob connectJob = vix.Connect(Constants.VIX_API_VERSION, Constants.VIX_SERVICEPROVIDER_VMWARE_VI_SERVER,
+                        "https://linc.nycapt35k.com/sdk/", 0, "vmuser", "admin123", 0, null, null);
+                    object[] connectProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
+                    object hosts = null;
+                    ulong rc = connectJob.Wait(connectProperties, ref hosts);
+                    if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
+                    IHost host = (IHost)((object[])hosts)[0];
 
                     {
-                        // connect to a VI host
-                        Console.WriteLine("Connecting to VI host");
-                        IJob connectJob = vix.Connect(Constants.VIX_API_VERSION, Constants.VIX_SERVICEPROVIDER_VMWARE_VI_SERVER,
-                            "https://linc.nycapt35k.com/sdk/", 0, "vmuser", "admin123", 0, null, null);
-                        object[] connectProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
-                        object hosts = null;
-                        ulong rc = connectJob.Wait(connectProperties, ref hosts);
+                        // open a vm
+                        Console.WriteLine("Opening VM");
+                        IJob openJob = host.OpenVM("[dbprotect-1] ddoub-red/ddoub-red.vmx", null);
+                        object[] openProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
+                        object openResults = null;
+                        rc = openJob.Wait(openProperties, ref openResults);
                         if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
-                        IHost host = (IHost)((object[])hosts)[0];
-
-                        {
-                            // open a vm
-                            Console.WriteLine("Opening VM");
-                            IJob openJob = host.OpenVM("[dbprotect-1] ddoub-red/ddoub-red.vmx", null);
-                            object[] openProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
-                            object openResults = null;
-                            rc = openJob.Wait(openProperties, ref openResults);
-                            if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
-                        }
-
-                        // disconnect
-                        Console.WriteLine("Disconnecting");
-                        host.Disconnect();
                     }
 
+                    // disconnect
+                    Console.WriteLine("Disconnecting");
+                    host.Disconnect();
+                }
+
+                {
                     // connect to a local Workstation
+                    Console.WriteLine("Connecting to Workstation local host");
+                    IJob connectJob = vix.Connect(Constants.VIX_API_VERSION, Constants.VIX_SERVICEPROVIDER_VMWARE_WORKSTATION, null, 0, null, null, 0, null, null);
+                    object[] connectProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
+                    object hosts = null;
+                    ulong rc = connectJob.Wait(connectProperties, ref hosts);
+                    if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
+                    IHost host = (IHost)((object[])hosts)[0];
 
                     {
-                        // connect to a VI host
-                        Console.WriteLine("Connecting to Workstation local host");
-                        IJob connectJob = vix.Connect(Constants.VIX_API_VERSION, Constants.VIX_SERVICEPROVIDER_VMWARE_WORKSTATION, null, 0, null, null, 0, null, null);
-                        object[] connectProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
-                        object hosts = null;
-                        ulong rc = connectJob.Wait(connectProperties, ref hosts);
+                        // open a vm
+                        Console.WriteLine("Opening VM");
+                        IJob openJob = host.OpenVM(@"c:\Users\ddoubro\Virtual Machines\Windows XP Pro SP3 25GB\WinXP Pro SP3 25GB.vmx", null);
+                        object[] openProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
+                        object openResults = null;
+                        rc = openJob.Wait(openProperties, ref openResults);
                         if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
-                        IHost host = (IHost)((object[])hosts)[0];
-
-                        {
-                            // open a vm
-                            Console.WriteLine("Opening VM");
-                            IJob openJob = host.OpenVM(@"c:\Users\ddoubro\Virtual Machines\Windows XP Pro SP3 25GB\WinXP Pro SP3 25GB.vmx", null);
-                            object[] openProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
-                            object openResults = null;
-                            rc = openJob.Wait(openProperties, ref openResults);
-                            if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
-                        }
-
-                        // disconnect
-                        Console.WriteLine("Disconnecting");
-                        host.Disconnect();
                     }
 
+                    // disconnect
+                    Console.WriteLine("Disconnecting");
+                    host.Disconnect();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
