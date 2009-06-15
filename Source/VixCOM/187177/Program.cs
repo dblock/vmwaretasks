7dd9d0e3aad8552e37587b7d6540c7af7226739c
Daniel Doubrovkine <dblock@dblock.org>
2009-06-15 23:41:31 +0000

    Proper disposal of IHost and IVM2 on exception.
    git-svn-id: https://vmwaretasks.svn.codeplex.com/svn/trunk@25170 9b283d60-5439-405e-af05-b73fd8c4d996

--- Source/VixCOM/187177/Program.cs
+++ Source/VixCOM/187177/Program.cs
@@ -13,21 +13,27 @@ namespace VMWareCrash
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            int iterationNo = 1;
             while (true)
             {
+                IHost host = null;
+                IVM2 vm = null;
+
                 try
                 {
                     VixCOM.VixLib vix = new VixLib();
-
+                    Console.WriteLine("**** Iteration: " + iterationNo + "*******");
+                    iterationNo++;
                     // connect to a VI host
                     Console.WriteLine("Connecting to host");
                     IJob connectJob = vix.Connect(Constants.VIX_API_VERSION, Constants.VIX_SERVICEPROVIDER_VMWARE_VI_SERVER,
                         "https://linc.nycapt35k.com/sdk/", 0, "vmuser", "admin123", 0, null, null);
+
                     object[] connectProperties = { Constants.VIX_PROPERTY_JOB_RESULT_HANDLE };
                     object hosts = null;
                     ulong rc = connectJob.Wait(connectProperties, ref hosts);
                     if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
-                    IHost host = (IHost)((object[])hosts)[0];
+                    host = (IHost)((object[])hosts)[0];
 
                     {
                         // open a vm
@@ -37,12 +43,15 @@ namespace VMWareCrash
                         object openResults = null;
                         rc = openJob.Wait(openProperties, ref openResults);
                         if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
-                        IVM2 vm = (IVM2)((object[])openResults)[0];
+                        vm = (IVM2)((object[])openResults)[0];
                         // get root snapshot
                         Console.WriteLine("Fetching root snapshot");
                         ISnapshot snapshot = null;
                         rc = vm.GetRootSnapshot(0, out snapshot);
-                        if (vix.ErrorIndicatesFailure(rc)) throw new Exception(vix.GetErrorText(rc, "en-US"));
+                        if (vix.ErrorIndicatesFailure(rc))
+                        {
+                            throw new Exception(vix.GetErrorText(rc, "en-US"));
+                        }
                         Console.WriteLine("Reverting to snapshot");
                         // revert to the snapshot
                         IJob revertJob = vm.RevertToSnapshot(snapshot, Constants.VIX_VMPOWEROP_NORMAL, null, null);
@@ -50,16 +59,29 @@ namespace VMWareCrash
                     }
 
                     // disconnect
-                    Console.WriteLine("Disconnecting");
-                    host.Disconnect();
-
-                    Console.WriteLine("GC");
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    if (null != vm)
+                    {
+						Console.WriteLine("Closing VM");
+                        vm.Close();
+                    }
+
+                    if (null != host)
+                    {
+                        Console.WriteLine("Disconnecting");
+                        host.Disconnect();
+                    }
+
+                    Console.WriteLine("GC");
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
                 }
             }
         }
