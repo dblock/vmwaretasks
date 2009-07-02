6c3e54f6a80f0f7e872245634b09c93168d1f182
Daniel Doubrovkine <dblock@dblock.org>
2009-07-02 12:57:44 +0000

    Added missing file.
    git-svn-id: https://vmwaretasks.svn.codeplex.com/svn/trunk@25682 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ Source/VMWareLibUnitTests/VMWareVirtualMachineLongTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Vestris.VMWareLib;
+using System.Configuration;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using VixCOM;
+
+namespace Vestris.VMWareLibUnitTests
+{
+    [TestFixture]
+    public class VMWareVirtualMachineLongTests
+    {
+        [Test]
+        public void TestRecordingBeginEnd()
+        {
+            if (!VMWareTest.RunWorkstationTests)
+                Assert.Ignore("Skipping, Workstation tests disabled.");
+
+            VMWareVirtualMachine virtualMachine = TestWorkstation.Instance.PoweredVirtualMachine;
+            Assert.IsFalse(virtualMachine.IsRecording);
+            string snapshotName = Guid.NewGuid().ToString();
+            Console.WriteLine("Begin recording ...");
+            VMWareSnapshot snapshot = virtualMachine.BeginRecording(snapshotName, Guid.NewGuid().ToString());
+            Assert.IsNotNull(snapshot);
+            Assert.IsTrue(virtualMachine.IsRecording);
+            Assert.IsFalse(virtualMachine.IsReplaying);
+            virtualMachine.WaitForToolsInGuest();
+            Console.WriteLine("Snapshot: {0}", snapshot.DisplayName);
+            VMWareVirtualMachine.Process cmdProcess = virtualMachine.RunProgramInGuest("cmd.exe", "/C dir");
+            Assert.IsNotNull(cmdProcess);
+            Console.WriteLine("Process: {0}", cmdProcess.Id);
+            Console.WriteLine("End recording ...");
+            virtualMachine.EndRecording();
+            Assert.IsFalse(virtualMachine.IsRecording);
+            Assert.IsFalse(virtualMachine.IsReplaying);
+            Console.WriteLine("Begin replay ...");
+            snapshot.BeginReplay(VixCOM.Constants.VIX_VMPOWEROP_LAUNCH_GUI, VMWareInterop.Timeouts.ReplayTimeout);
+            Assert.IsTrue(virtualMachine.IsReplaying);
+            Thread.Sleep(10000);
+            snapshot.EndReplay();
+            Assert.IsFalse(virtualMachine.IsReplaying);
+            Console.WriteLine("Removing snapshot ...");
+            snapshot.RemoveSnapshot();
+        }
+
+        [Test]
+        public void TestReset()
+        {
+            foreach (VMWareVirtualMachine virtualMachine in VMWareTest.PoweredVirtualMachines)
+            {
+                // hardware reset
+                Console.WriteLine("Reset ...");
+                virtualMachine.Reset();
+                Assert.AreEqual(true, virtualMachine.IsRunning);
+                Console.WriteLine("Wait ...");
+                virtualMachine.WaitForToolsInGuest();
+            }
+        }
+    }
+}
