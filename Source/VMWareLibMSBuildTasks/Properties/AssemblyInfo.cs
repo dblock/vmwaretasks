e84e51cac7853da05fb89af4f997eb9f482d7b18
Daniel Doubrovkine <dblock@dblock.org>
2010-01-26 23:01:09 +0000

    A skeleton for VMWareLib MSBuild tasks with a sample connect task.
    git-svn-id: https://vmwaretasks.svn.codeplex.com/svn/trunk@39198 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ Source/VMWareLibMSBuildTasks/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VMWareLibMSBuildTasks")]
+[assembly: AssemblyDescription("VMWareTasks MSBuild Library")]
+[assembly: Guid("03069325-0041-44ab-807a-de0e73ba55d1")]
+[assembly: ComVisible(false)]
